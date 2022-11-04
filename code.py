a78435b97c652146dc3a25d6748c94e309cd3a6e
Troy-Appleby <113041178+Troy-Appleby@users.noreply.github.com>
2022-11-04 10:04:22 -0400

    code

--- code.py
+++ code.py
@@ -7,41 +7,59 @@
 import ugame
 import stage
 
+import constants
+
 
 def game_scene():
     # this function is the main game game_scene
 
-    # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
-    image_bank_background = stage.Bank.from_bmp16("space_aliens.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # set the background to image 0 in the image bank
-    #   and the size (10x8 tiles of size 16x16)
-    background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
 
-    # a sprite that will be updated every frame
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
 
-    # create a stage for the background to show up on
-    #   and set the frame rate to 60fps
-    game = stage.Stage(ugame.display, 60)
-
-    # set the layers of all sprites, items show up in order
+    game = stage.Stage(ugame.display, constants.FPS)
     game.layers = [ship] + [background]
-
-    # render all sprites
-    #   most likely you will only render the background once per scene
     game.render_block()
 
-    # repeat forever, game loop
     while True:
         # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            pass
+        if keys & ugame.K_O:
+            pass
+        if keys & ugame.K_START:
+            pass
+        if keys & ugame.K_SELECT:
+            pass
+        if keys & ugame.K_RIGHT:
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 1, ship.y)
+            else:
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
+        if keys & ugame.K_LEFT:
+            if ship.x >= 0:
+                ship.move(ship.x - 1, ship.y)
+            else:
+                ship.move(0, ship.y)
+        if keys & ugame.K_UP:
+            pass
+        if keys & ugame.K_DOWN:
+            pass
 
         # update game logic
 
-        # redraw Sprite
+        # redraw Sprites
         game.render_sprites([ship])
-        game.tick() # wait untill refresh rate finishes
+        game.tick()  # wait until refresh rate finishes
 
 
 if __name__ == "__main__":
