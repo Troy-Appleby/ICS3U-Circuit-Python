808a49a8235ef825fdf78842af9b2ee46e4b6a63
Troy-Appleby <113041178+Troy-Appleby@users.noreply.github.com>
2022-11-14 08:16:24 -0800

    code

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 
 # Created by: Troy Appleby
-# Created on: Oct 2022
+# Created on: November 2022
 # This constants file is for Space Alien game
 
 # PyBadge screen size is 160x128 and sprites are 16x16
@@ -21,3 +21,8 @@ button_state = {
     "button_still_pressed": "still pressed",
     "button_released": "released",
 }
+
+# new pallet for red filled text
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+            
