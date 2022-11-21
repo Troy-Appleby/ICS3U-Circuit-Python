d2b9c610d16a47ba886366755eaabc32ced9fa01
Troy-Appleby <113041178+Troy-Appleby@users.noreply.github.com>
2022-11-21 10:48:35 -0500

    code

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by: Troy Appleby
-# Created on: Oct 2022
+# Created on: Nov 2022
 # This program is the "Space Aliens" program on the PyBadge.
 
 import random
@@ -15,8 +15,7 @@ import ugame
 def splash_scene():
     # this function is the splash scene game loop
 
-    # get sound ready
-    coin_sound = open("con.wav", "rb")
+    # get sound ready        coin_sound = open("con.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
@@ -138,9 +137,12 @@ def game_scene():
     sound.mute(False)
 
     # set the background to image 0 in the image bank
-    background = stage.Grid(
-        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
-    )
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X,
+        constants.SCREEN_GRID_Y)
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
 
     # a sprite that will be updated every frame
     ship = stage.Sprite(
@@ -154,12 +156,21 @@ def game_scene():
         16,
     )
 
+    # create list of lasers for when we shoot
+    lasers = []
+    for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
+        a_single_laser = stage.Sprite(image_bank_sprites, 10,
+        constants.OFF_SCREEN_X,
+        constants.OFF_SCREEN_Y)
+        lasers.append(a_single_laser)
+
+
     # create a stage for the background to show up on
     #  and set the frame rate to 60 fps
     game = stage.Stage(ugame.display, constants.FPS)
 
     # set the layers of all sprites, items show up in order
-    game.layers = [ship] + [alien] + [background]
+    game.layers = lasers + [ship] + [alien] + [background]
 
     # render all sprites
     game.render_block()
@@ -172,7 +183,7 @@ def game_scene():
             if a_button == constants.button_state["button_up"]:
                 a_button = constants.button_state["button_just_pressed"]
             elif a_button == constants.button_state["button_just_pressed"]:
-                a_button = constants.button_state["button_still_pressed"]
+               a_button = constants.button_state["button_still_pressed"]
         else:
             if a_button == constants.button_state["button_still_pressed"]:
                 a_button = constants.button_state["button_released"]
@@ -201,10 +212,26 @@ def game_scene():
             pass
 
         if a_button == constants.button_state["button_just_pressed"]:
-            sound.play(pew_sound)
+            # fire a laser, if we have enough power (have not used up all the lasers)
+            for laser_number in range(len(lasers)):
+                if lasers[laser_number].x < 0:
+                    lasers[laser_number].move(ship.x, ship.y)
+                    sound.play(pew_sound)
+                    break
 
-        game.render_sprites([ship] + [alien])
-        game.tick()
+        # each frame move the lasers, that have been fired up 
+        for laser_number in range(len(lasers)):
+            if lasers[laser_number].x > 0:
+                lasers[laser_number].move(lasers[laser_number].x,
+                lasers[laser_number].y -
+                constants.LASER_SPEED)
+                if lasers[laser_number].y < constants.OFF_TOP_SCREEN:
+                    lasers[laser_number].move(constants.OFF_SCREEN_X,
+                    constants.OFF_SCREEN_Y)
+
+        # redraw Sprite
+        game.render_sprites(lasers + [ship] + [alien])
+        game.tick() # wait until rfresh rate finishes
 
 
 if __name__ == "__main__":
--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by: Troy Appleby
-# Created on: November 2022
+# Created on: Oct 2022
 # This constants file is for Space Alien game
 
 # PyBadge screen size is 160x128 and sprites are 16x16
@@ -11,6 +11,14 @@ SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
+TOTAL_NUMBER_OF_LASERS = 5
+SHIP_SPEED = 1
+ALIEN_SPEED = 1
+LASER_SPEED = 2
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
 
@@ -19,10 +27,9 @@ button_state = {
     "button_up": "up",
     "button_just_pressed": "just pressed",
     "button_still_pressed": "still pressed",
-    "button_released": "released",
+    "button_released": "released"
 }
 
 # new pallet for red filled text
 RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
                b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
-            
